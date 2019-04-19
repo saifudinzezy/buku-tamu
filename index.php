6e6832e4a582baa90f7e8c8568c3ce3ba67decd7
saifudin <saifudin.zezy7@gmail.com>
2019-04-19 20:21:52 +0700

    test

--- index.php
+++ index.php
@@ -10,18 +10,18 @@
      <h1>Registrasi Buku Tamu!</h1>
      <form method="post" action="index.php" enctype="multipart/form-data" >
       <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <label for="exampleInputEmail1">Nama</label>
+        <input type="text" class="form-control" placeholder="Masukan Nama" name="nama">        
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <label for="exampleInputPassword1">No HP</label>
+        <input type="text" class="form-control" id="exampleInputPassword1" placeholder="NO HP" name="tlp">
       </div>
-      <div class="form-group form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Berkunjung ke rumah</label>
+        <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Kepala Keluarga" name="kpl">
       </div>
+     
         <input type="submit" name="submit" value="Submit" class="btn btn-primary" />
         <input type="submit" name="load_data" value="Load Data" class="btn btn-warning" />
     </form>
