822c96a669d113ab5c5cc9a0077af93628fdc036
saifudin <saifudin.zezy7@gmail.com>
2019-04-19 20:18:42 +0700

    div

--- index.php
+++ index.php
@@ -6,25 +6,26 @@
     <script type="text/javascript" src="js/bootstrap.js"></script>
  </head>
  <body>
- <h1>Registrasi Buku Tamu!</h1>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-    <input type="submit" name="submit" value="Submit" class="btn btn-primary" />
-    <input type="submit" name="load_data" value="Load Data" class="btn btn-warning" />
-</form>
-
+<div class="container">
+     <h1>Registrasi Buku Tamu!</h1>
+     <form method="post" action="index.php" enctype="multipart/form-data" >
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+        <input type="submit" name="submit" value="Submit" class="btn btn-primary" />
+        <input type="submit" name="load_data" value="Load Data" class="btn btn-warning" />
+    </form>
+</div>
  <?php
     $host = "registrasi.database.windows.net";
     $user = "saifudin";
